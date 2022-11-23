bf07190e39cdf2286d6ec4996ee09a7bd2e831d0
Saurabh <saurabhsuchak1@gmail.com>
2022-11-23 20:39:37 +0530

    updated

--- Programming_Assignment/string_edit/dynamic_prog.cpp
+++ Programming_Assignment/string_edit/dynamic_prog.cpp
@@ -1,22 +1,3 @@
-# String Editing
-<br/>
-
-### Problem Statement:
-Implement the string editing algorithm using dynamic programming approach and demonstrate 3 test cases.
-
-
-
-### Applications of String Editing :
-String edit distances have been used for decades and has various applications with some them being -
-- Spelling correction
-- Web search suggestions. For example, the Apache Lucene
-- DNA analysis like DNA-protein matching and sequence assembly
-<br>
-
-
-
-### Code:
-```
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -55,24 +36,3 @@ int main(){
         
 }
         
-
-```
-
-### Testcase 1 :
-```
-
-```
-
-### Testcase 2 :
-```
-
-```
-
-### Time and Space complexity :
-```
-Time Complexity of Recursion - O(min(3^n1, 3^n2)) // O(min(3^(length string1), 3^(length string2))
-Space Complexity - O(n)    // stack space for recursion
-
-Time Complexity in dynamic prog. -  O(n1 * n2)
-Space Complexity - O(n1 * n2)
-```
